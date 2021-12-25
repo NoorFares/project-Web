34fbb4e031a091d36bb566bcb9083e2eeee37842
KhalidKhffaush <92662422+Khalidfathi@users.noreply.github.com>
2021-12-25 10:31:59 +0200

    Update bouns.js

--- bouns.js
+++ bouns.js
@@ -1,1 +1,12 @@
-/////////////////////////////////////////////
+function game() {
+    const actions = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+    const userWinResults = ['scissorspaper', 'paperrock', 'rocklizard', 'lizardspock', 'spockscissors',
+    'rockscissors', 'scissorslizard', 'lizardpaper', 'paperspock', 'spockrock'];
+    let userChoice = '';
+    let compChoice = '';
+    const resultTitleElement = resultElement.querySelector('.title');
+    const scoreCountElement = document.querySelector('.score-count');
+
+    let currentScore = null;
+}
+game();
