a11a735498eec6c83b9bb41479079e62108f65ef
KhalidKhffaush <92662422+Khalidfathi@users.noreply.github.com>
2021-12-25 10:30:29 +0200

    Update 1player.js

--- 1player.js
+++ 1player.js
@@ -1,1 +1,32 @@
-/////////////////////////////
+setTimeout(() => {
+    document.body.classList.remove("preload");
+  }, 500);
+  
+  const CHOICES = [
+    {
+      name: "paper",
+      beats: "rock",
+    },
+    {
+      name: "scissors",
+      beats: "paper",
+    },
+    {
+      name: "rock",
+      beats: "scissors",
+    },
+  ];
+  
+  const scoreNumber = document.querySelector(".score__number");
+  let score = 0;
+ 
+  function isWinner(results) {
+    return results[0].beats === results[1].name;
+  }
+  
+  function keepScore(point) {
+    score += point;
+    scoreNumber.innerText = score;
+  }
+  
+ 
