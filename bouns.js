fb887f5de8144c46b01a7af0400d5b8c68235c8f
git config --global user.namNoorFares <s12028537@stu.najah.edu>
2021-12-25 11:22:12 +0200

    Update bouns.js

--- bouns.js
+++ bouns.js
@@ -1,1 +1,37 @@
-/////////////////////////////////////////////
+function game() {
+    const actions = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+    const userWinResults = ['scissorspaper', 'paperrock', 'rocklizard', 'lizardspock', 'spockscissors',
+    'rockscissors', 'scissorslizard', 'lizardpaper', 'paperspock', 'spockrock'];
+    let userChoice = '';
+    let compChoice = '';
+    const userChoiceElement = document.querySelector('.user-choice');
+    const pickedElement = document.querySelector('.picked');
+    const userPickElement = document.querySelector('.user-pick');
+    const pcPickElement = document.querySelector('.pc-pick');
+    window.addEventListener('load', () => {
+    
+        document.querySelectorAll('.user-choice .game-card').forEach(card => {
+            card.addEventListener('click', (ev) => {
+                userChoice = getUserChoice(ev.target);
+                compChoice = getComputerChoice();
+    
+                startGame();
+            })
+        });
+       
+        
+        resultElement.querySelector('button').addEventListener('click', tryAgain);
+    
+    })
+    
+   
+    function getUserChoice(target) {
+        if (target.nodeName === 'IMG') {
+            return target.parentElement.classList[1];
+        }
+        return target.classList[1];
+    }
+    
+    function getComputerChoice() {
+        return actions[Math.floor(Math.random() * 5)];
+    }}
